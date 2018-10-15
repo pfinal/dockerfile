0f3b7eb6ea910b16bc5692cbdb5bfec58c6ae5b5
Ethan <nicesoft333@qq.com>
2018-10-15 21:43:51 +0800

    flask

---
+++ flask/myapp.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import redis
+
+app = Flask(__name__)
+
+@app.route('/')
+def heloo():
+    return 'Hello Flask!'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
