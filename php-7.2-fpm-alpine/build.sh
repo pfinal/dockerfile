6cc3075730dbe22c4016c2cdefb18509e6b5fa6d
Ethan <nicesoft333@qq.com>
2018-10-17 16:33:39 +0800

    dockerfile

--- php-7.2-fpm-alpine/build.sh
+++ php-7.2-fpm-alpine/build.sh
@@ -1,8 +1,5 @@
-docker build -t local/php:7.2-fpm-alpine .
+docker build -t pfinal/php:7.2-fpm-alpine .
 
 # docker login
-# docker tag local/php:7.2-fpm-alpine 190196539/php:7.2-fpm-alpine
-# git push 190196539/php:7.2-fpm-alpine
-# docker push 190196539/php:7.2-fpm-alpine
-# docker push 190196539/php:7.2-fpm-alpine
+docker push pfinal/php:7.2-fpm-alpine
 
